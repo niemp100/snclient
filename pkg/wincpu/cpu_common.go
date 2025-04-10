349edf10cfe4875c4574ff1765e99aba0564dc73
Niklas Empt <niklas.empt@consol.de>
2025-04-10 13:36:52 +0200

    linter line

--- pkg/wincpu/cpu_common.go
+++ pkg/wincpu/cpu_common.go
@@ -10,5 +10,5 @@ import (
 
 func Percent(interval time.Duration, percpu bool) ([]float64, error) {
 	// On non windows system invoke library funtions
-	return cpuinfo.Percent(interval, percpu)
+	return cpuinfo.Percent(interval, percpu) //nolint:wrapcheck // Temporary Lib replacement
 }
